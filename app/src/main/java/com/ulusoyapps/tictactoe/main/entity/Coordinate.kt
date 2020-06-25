53d2d47cd2502e869bd1d311c7c572651b40a5ce
ulusoyca <cagatay.ulusoy@amersports.com>
2020-06-26 01:45:11 +0300

    add tic tac toe table

---
+++ app/src/main/java/com/ulusoyapps/tictactoe/main/entity/Coordinate.kt
@@ -0,0 +1,6 @@
+package com.ulusoyapps.tictactoe.main.entity
+
+data class Coordinate(
+    val row: Int,
+    val column: Int
+)
---
+++ app/src/main/java/com/ulusoyapps/tictactoe/main/ui/TicTacToeBox.kt
@@ -0,0 +1,34 @@
+package com.ulusoyapps.tictactoe.main.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
+import com.ulusoyapps.tictactoe.R
+import com.ulusoyapps.tictactoe.main.entity.Coordinate
+
+class TicTacToeBox
+@JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : MaterialButton(context, attrs) {
+
+    var coordinate = Coordinate(0, 0)
+
+    init {
+        gravity = Gravity.CENTER
+        onComputerMove()
+        onPlayerMove()
+    }
+
+    fun onComputerMove() {
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.green)
+        icon = context.getDrawable(R.drawable.ic_cross)
+    }
+
+    fun onPlayerMove() {
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.gray)
+        icon = context.getDrawable(R.drawable.ic_circle)
+    }
+}
