c115cb985b7436d92aab2b3378664d7be10e26a7
ulusoyca <cagatay.ulusoy@amersports.com>
2020-06-29 01:44:25 +0300

    add splah logic

--- app/src/main/java/com/ulusoyapps/tictactoe/main/splash/SplashFragment.kt
+++ app/src/main/java/com/ulusoyapps/tictactoe/main/splash/SplashFragment.kt
@@ -1,12 +1,15 @@
 package com.ulusoyapps.tictactoe.main.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ulusoyapps.tictactoe.databinding.FragmentSplashBinding
 import dagger.android.support.DaggerFragment
+
+private const val START_DELAY = 2000L
 
 class SplashFragment : DaggerFragment() {
 
@@ -23,8 +26,12 @@ class SplashFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.root.setOnClickListener {
-            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
-        }
+        Handler().postDelayed(
+            { startHomeFragment() }, START_DELAY
+        )
+    }
+
+    private fun startHomeFragment() {
+        findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
     }
 }
